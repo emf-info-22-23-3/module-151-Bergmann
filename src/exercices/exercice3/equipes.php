99488da1867c74418b7b2ad1ef1167a9e66e9f08
BergmannL <BergmannL@studentfr.ch>
2025-01-27 12:52:01 +0100

    exercice 3

--- src/exercices/exercice3/equipes.php
+++ src/exercices/exercice3/equipes.php
@@ -1,21 +1,32 @@
 <!doctype html>
 <html>
-  <header>
-    <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
+<header>
+  <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
 </header>
-  <body>
-    <div id="conteneur">
-      <h1>Les équipes de National League</h1>    
-      <table border= "1">
+
+<body>
+  <div id="conteneur">
+    <h1>Les équipes de National League</h1>
+    <table border="1">
       <tr>
         <td>ID</td>
         <td>Club</td>
       </tr>
       <?php
-        require('ctrl.php');
-        // A compléter....
+      require('ctrl.php');
+      $liste = getEquipes();
+      
+      for ($i = 0 ; $i < sizeof($liste) ; $i++)
+      {
+        echo "<tr>";
+        echo "<td>" . $i . "</td>";
+        echo "<td>" . $liste[$i] . "</td>";
+        echo "</tr>";
+      }
+
       ?>
-      </table>
-    </div>
-  </body>
+    </table>
+  </div>
+</body>
+
 </html>
