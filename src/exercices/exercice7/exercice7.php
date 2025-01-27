f538cc25ec276a8369abf3b7b018cb12c1ef561f
BergmannL <BergmannL@studentfr.ch>
2025-01-27 13:43:48 +0100

    exercice 7

--- src/exercices/exercice7/exercice7.php
+++ src/exercices/exercice7/exercice7.php
@@ -1,12 +1,12 @@
 <?php
 	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = …;
+	$reponse = $bdd->query('SELECT titre FROM jeux_video');
 	
 	
-	while (…)
+	while ($row = $response->fetch())
 	{
 	
-	        …
+		echo <p>$row['titre']</p>;
 
 	
 	}
