739e7be70c61c6aea599e0aba5e1d19dfbe12686
BergmannL <BergmannL@studentfr.ch>
2025-01-27 13:25:49 +0100

    exercice 5

--- src/exercices/exercice5/client/javascripts/services/servicesHttp.js
+++ src/exercices/exercice5/client/javascripts/services/servicesHttp.js
@@ -5,7 +5,7 @@
  * @version 1.0 / 20-SEP-2013
  */
 
-var BASE_URL = "METTEZ L'URL D'ADRESSE DU PHP DU SERVEUR";
+var BASE_URL = "http://localhost:8080/Project151/module-151-LeonBergmannEMF/src/exercices/exercice5/Server/joueurs.php";
 
 /**
  * Fonction permettant de charger les données d'équipe.
