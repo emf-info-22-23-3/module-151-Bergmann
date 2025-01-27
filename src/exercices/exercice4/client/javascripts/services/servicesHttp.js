6cfab16f9079aa4f9495bf99f06daab3945d5450
BergmannL <BergmannL@studentfr.ch>
2025-01-27 12:58:24 +0100

    exercice 4

--- src/exercices/exercice4/client/javascripts/services/servicesHttp.js
+++ src/exercices/exercice4/client/javascripts/services/servicesHttp.js
@@ -5,7 +5,7 @@
  * @version 1.0 / 20-SEP-2013
  */
 
-var BASE_URL = "mettez ici l'adresse du PHP que vous souhaitez appeler";
+var BASE_URL = "http://localhost:8080/Project151/module-151-LeonBergmannEMF/src/exercices/exercice4/Server/serveur.php";
 
 /**
  * Fonction permettant de charger les données d'équipe.
